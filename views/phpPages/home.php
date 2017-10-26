5c416798535bc79ff6a5cfb946c2d08032290582
Bridget Bourne <bbourne8891@hotmail.com>
2017-10-25 23:35:57 -0400

    Need to convert js mysql data to php mysql data in home page to get signing posts to work

--- views/phpPages/home.php
+++ views/phpPages/home.php
@@ -8,6 +8,40 @@
 
 	<?php 
 	//The "method" specified in the form already posted the login data to the server. Now we respond accordingly.
+	// //Making queries to MySQL to add login data where necessary.
+	// function signup(){
+	// 	//Connecting to MySQL for database access
+
+	// 	var connection = mysql.createConnection({
+	// 	  host: "localhost",
+	// 	  port: 3306,
+
+	// 	  // Your username
+	// 	  user: "root",
+
+	// 	  // Your password
+	// 	  password: "",
+	// 	  database: "User"
+	// 	});//End connection variable definition
+
+	// 	connection.connect(function(err) { //Keep queries inside this object.
+	// 		if (err) throw err;
+	// 		console.log("connected as id " + connection.threadId + "\n");
+	// 		insertCrud();//Function TBD
+	// 		//Connecting mySQL queries to proper HTML elements
+	// 		var loginDrop = "DROP TABLE `login` IF EXISTS"
+	// 		var loginInsert = "INSERT INTO `Login` (first_name, last_name, username, email, password, password_hint) VALUES ?"; //"SET ?" meaning... "WHERE ?" meaning...
+	// 		var loginValues = [
+	// 		//This is where users' input in login form will be entered. Find proper MySQL functions
+	// 		//It'd be nice if I could summon data from the demonstration example. But how? Hmmmm...
+	// 		];
+	// 		connection.query(
+	// 			loginDrop, loginInsert, [loginValues], function(err, result){
+	// 				if (err) throw err;
+	// 				console.log("Number of rows affected in `Login`: ", result.affectedRows);
+	// 			});//End connection.query
+	// 	});//End connection.connect		
+	// };//End function signup
 	if(
 		$_POST["username"] === /*MySQL username here*/ && $_POST["password"] === /*MySQL username here*/){
 		header("Location: localhost:7000/weeklySchedule");
